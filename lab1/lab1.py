3c33d4f155f0fcfacc76fbe099947548fc787a22
reyimov70 <reyimovv7@gmail.com>
2026-01-26 15:53:29 +0500

    lab1

---
+++ lab1/lab1.py
@@ -0,0 +1,11 @@
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+
+sum_result = a + b
+diff_result = a - b
+prod_result = a * b
+
+print("Сумма:", sum_result)
+print("Разность:", diff_result)
+print("Произведение:", prod_result)
+print("Произведение:", prod_result)
